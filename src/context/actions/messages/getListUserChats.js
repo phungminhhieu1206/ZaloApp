416dadf4454f30778c22f57b0a7e3ab53548a6ef
Đinh Tiến Vũ <vuthptnhoquana@gmail.com>
2022-01-09 11:42:17 +0700

    Vu: add list post

---
+++ src/context/actions/messages/getListUserChats.js
@@ -0,0 +1,37 @@
+import {
+    GET_CHAT_LIST_LOADING,
+    GET_CHAT_LIST_SUCCESS,
+    GET_CHAT_LIST_FAIL
+} from "../../../constants/actionTypes"
+
+import axiosInstance from "../../../helpers/axiosInstance";
+
+const getListUserChats = () => (dispatch) => {
+    dispatch({
+        type: GET_CHAT_LIST_LOADING,
+    });
+    axiosInstance.get('/chats/listUser', {}).then((res) => {
+        // let temp = JSON.parse(res.request._response).data.friends
+        let resData = res.json();
+        let data = resData.data;
+        
+        
+        dispatch({
+            type: GET_CHAT_LIST_SUCCESS,
+            payload: data 
+            /**
+             * Để ý dữ liệu payload ở đây truyền vào reducer phải giống với định dạng của data initialState
+             */
+        });
+    }).catch((err) => {
+        console.log('list chats error -->', err.response);
+        dispatch({
+            type: GET_CHAT_LIST_FAIL,
+            payload: err.response
+                ? err.response.data
+                : { error: 'No Internet, try again !' },
+        });
+    })
+}
+
+export default getListUserChats;
---
+++ src/context/initialStates/postsInitialState.js
@@ -0,0 +1,7 @@
+export default {
+    getListPosts: {
+        data: [],
+        error: null,
+        loading: false,
+    }
+}
